70b608be046d09f2136f478a6ca337c534bd188a
Kirill Granovsky <dj-kol@yandex.ru>
2024-09-06 15:12:19 +0300

    added Main example

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Kirill", "89629248553");
+        phoneBook.add("Andrey", "89629248552");
+        phoneBook.add("Zoiya", "89629248551");
+
+        System.out.println("По имени Kirill, найден(а) телефон " +phoneBook.findByName("Kirill"));
+        System.out.println("По номеру 89629248551, найден(а) контакт " + phoneBook.findByNumber("89629248551"));
+        phoneBook.printAllNames();
+
     }
 }
