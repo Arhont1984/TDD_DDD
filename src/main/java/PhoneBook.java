cf8dffe5ab2a20e652e288d4372bb2e1084ead36
Kirill Granovsky <dj-kol@yandex.ru>
2024-09-06 14:12:41 +0300

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,16 @@
+import java.util.HashMap;
+import java.util.TreeSet;
+
 public class PhoneBook {
+
+    // Хранение имен и номеров телефонов
+    HashMap<String, String> contacts = new HashMap<>();
+    // Хранение имен в алфавитном порядке
+    TreeSet<String> names = new TreeSet<>();
+
+
+    public int add(String name, String number) {
+        return 0;
+    }
+
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,13 @@
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
 public class PhoneBookTest {
+
+    @Test
+    public void add() {
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Kirill", "89629248553");
+        Assertions.assertTrue(phoneBook.contacts.containsKey("Kirill") && phoneBook.contacts.containsValue("89629248553"));
+    }
+
 }
